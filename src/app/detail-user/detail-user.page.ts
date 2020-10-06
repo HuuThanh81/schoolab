c3078bb5dc8040e73cd69bf6bc855d16d7c0d28a
HuuThanh81 <huynhlehuuthanh@gmail.com>
2020-10-06 07:46:09 +0700

    update ui

--- src/app/detail-user/detail-user.page.ts
+++ src/app/detail-user/detail-user.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-detail-user',
@@ -7,13 +8,15 @@ import { Observable } from 'rxjs';
   styleUrls: ['./detail-user.page.scss'],
 })
 export class DetailUserPage implements OnInit {
-  name: string; email: string; password: string; amount: string;
+  name: string; email: string; password: string; amount: number; tran: string; chang: string; 
   state$: Observable<object>;
-  constructor() {
+  constructor(public toastController: ToastController) {
+    this.tran = 'Transfer';
+    this.chang = 'Recharge';
     this.email = history.state.email;
     this.name = history.state.name;
     this.password = history.state.password;
-    this.amount = history.state.amount;
+    this.amount = Number(history.state.amount);
   }
 
   ngOnInit() {
